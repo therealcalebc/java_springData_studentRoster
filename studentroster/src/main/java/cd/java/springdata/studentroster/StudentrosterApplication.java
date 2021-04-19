9fd8b1b1d7c5b8cf3d78cd0867ce2480f4f386c2
Caleb A Comstock <c_comstock@rocketmail.com>
2021-04-19 10:17:06 -0700

    added initial project files

---
+++ studentroster/src/main/java/cd/java/springdata/studentroster/StudentrosterApplication.java
@@ -0,0 +1,13 @@
+package cd.java.springdata.studentroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentrosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentrosterApplication.class, args);
+	}
+
+}
